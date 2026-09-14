93077cc688de48b87ce8a7f30d89f7c8aadd705c
Arsam <arsam4waffels@gmail.com>
2026-09-14 15:05:47 +0330

    fix: return string instead of array address

--- src/main/java/com/mirosync/PasswordManager.java
+++ src/main/java/com/mirosync/PasswordManager.java
@@ -7,17 +7,16 @@ import java.security.NoSuchAlgorithmException;
 public class PasswordManager {
 
     public String hashPassword(String password) {
-
         try {
-
             MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-
             byte[] hashBytes = messageDigest.digest(
                     password.getBytes(StandardCharsets.UTF_8)
             );
-
-            return String.format("%02x", hashBytes);
-
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte bytes : hashBytes) {
+                stringBuilder.append(String.format("%02x", bytes));
+            }
+            return stringBuilder.toString();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
