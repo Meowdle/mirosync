16d9009941131ffe74ba1687250e2a62bc7d51c1
Arsam <arsam4waffels@gmail.com>
2026-09-14 17:47:34 +0330

    test: Analysis of the software's internal system

--- src/main/java/com/mirosync/Main.java
+++ src/main/java/com/mirosync/Main.java
@@ -1,5 +1,42 @@
 package com.mirosync;
 
+import java.io.Console;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        File config = new File("config.properties");
+        PasswordManager passwordManager = new PasswordManager();
+        FolderManager folderManager = new FolderManager(null);
+
+        // It is better than the 'Scanner', because it keeps the passwords hidden.
+        Console console = System.console();
+
+        if (!config.exists()) {
+
+            char[] password = console.readPassword();
+            String passwordString = new String(password);
+            passwordManager.savePassword(
+                    passwordManager.hashPassword(passwordString)
+            );
+            folderManager.createFolder();
+            folderManager.hideFolder();
+        }
+        else {
+            char[] password = console.readPassword();
+            String passwordString = new String(password);
+            if (Objects.equals(
+                    passwordManager.hashPassword(passwordString),
+                    passwordManager.loadPassword())
+            ) {
+                folderManager.showFolder();
+
+                System.out.println("Press [Enter] to lock folder...");
+                console.readLine(); folderManager.hideFolder();
+            }
+            else System.err.println("Wrong password.");
+        }
+    }
 }
