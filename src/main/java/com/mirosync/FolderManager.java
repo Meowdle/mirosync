7b0c699fd92cd25c82e1250a308e3b23f1a7585c
Arsam <arsam4waffels@gmail.com>
2026-09-14 16:54:39 +0330

    feat: Add support for handling null and empty values

--- src/main/java/com/mirosync/FolderManager.java
+++ src/main/java/com/mirosync/FolderManager.java
@@ -6,11 +6,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FolderManager {
+    // Default folder storage path
+    private final String DEFAULT_PATH = "C:/Users/Mirosync";
+
     public void createFolder(String pathAddress) {
+        // If a preferred address is not provided, use the default.
+        if (pathAddress == null || pathAddress.isEmpty())
+            pathAddress = DEFAULT_PATH;
+        // Path for creating a folder
         Path path = Paths.get(pathAddress);
         try {
             Files.createDirectory(path);
-        } catch (IOException e) {
+        } // Error while creating folder
+        catch (IOException e) {
+            // TODO : Improved error handling during retries
             throw new RuntimeException(e);
         }
     }
