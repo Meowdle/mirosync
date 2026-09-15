76cf83855799b9db47edc271b98db0f75e4f5ef6
Arsam <arsam4waffels@gmail.com>
2026-09-15 11:04:03 +0330

    refactor: core of Mirosync was refactored

--- src/main/java/com/mirosync/Main.java
+++ src/main/java/com/mirosync/Main.java
@@ -1,56 +1,7 @@
 package com.mirosync;
 
-import com.mirosync.folder.FolderManager;
-import com.mirosync.password.PasswordManager;
-
-import java.io.File;
-import java.util.Scanner;
-
 public class Main {
-    private static File config;
-    private static PasswordManager passwordManager;
-    private static FolderManager folderManager;
     public static void main(String[] args) {
-        config = new File("config.properties");
-        passwordManager = new PasswordManager();
-        folderManager = new FolderManager(null);
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome page");
-
-        if (!config.exists() || folderManager.exists()) {
-
-            System.out.println("Set a password");
-
-            String password = scanner.next();
-            if (password == null) {
-                System.err.println("Please use a terminal");
-                return;
-            }
-            passwordManager.savePassword(
-                    passwordManager.hashPassword(password)
-            );
-            folderManager.createFolder();
-            folderManager.hideFolder();
-        }
-        else {
-            int retryCount = 3;
-            while (retryCount != 0) {
-                retryCount = retryCount - 1;
-                String password = scanner.next();
-                if (passwordManager.passwordValidator(password)) {
-                    System.err.println("Folder is unlocked");
-                    folderManager.showFolder();
-                    System.out.println("Press any key to lock folder...");
-                    scanner.next();
-                    System.err.println("Folder is locked");
-                    folderManager.hideFolder();
-                    return;
-                }
-                System.out.println("try left : " + retryCount);
-                System.err.println("Wrong password. Try again.");
-            }
-        }
     }
 }
--- src/main/java/com/mirosync/Mirosync.java
+++ src/main/java/com/mirosync/Mirosync.java
@@ -1,10 +1,58 @@
 package com.mirosync;
 
+import com.mirosync.folder.FolderManager;
+import com.mirosync.password.PasswordManager;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class Mirosync {
-    public void start() {
-
+    private final String path;
+    public Mirosync(String path) {
+        this.path = path;
     }
-    public void setPath() {
+    public void start() {
+        FolderManager folderManager = new FolderManager(
+                path == null ? null : path
+        );
+        PasswordManager passwordManager = new PasswordManager();
+        File files = new File("config.properties");
+        System.out.println("..:: Mirosync Protector ::..");
 
+        Scanner scanner = new Scanner(System.in);
+        if (!files.exists()) {
+            System.out.println("First boost detected. Please choose a password : ");
+
+            String password = scanner.next();
+            if (password == null) {
+                System.err.println("Please use a terminal");
+                return;
+            }
+            passwordManager.savePassword(
+                    passwordManager.hashPassword(password)
+            );
+            folderManager.createFolder();
+            folderManager.hideFolder();
+        }
+        else {
+            int retryCount = 3;
+            while (retryCount != 0) {
+                retryCount--;
+                String password = scanner.next();
+                if (passwordManager.passwordValidator(password)) {
+                    System.err.println("Folder is unlocked");
+                    folderManager.showFolder();
+                    System.out.println("Press any key to lock folder...");
+                    scanner.next();
+                    System.err.println("Folder is locked");
+                    folderManager.hideFolder();
+                    return;
+                }
+                System.out.println("try left : " + retryCount);
+                System.err.println("Wrong password. Try again.");
+            }
+        }
     }
 }
